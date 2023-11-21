3987d91fd6b43f7d966391ca83c2683a4c850432
Amina Fazlić <fazlicamina02@gmail.com>
2023-11-21 14:20:05 +0100

    Dodavanje finalnog UI-a

--- PinkTacToe/src/app/board/board.component.ts
+++ PinkTacToe/src/app/board/board.component.ts
@@ -23,7 +23,7 @@ export class BoardComponent implements OnInit{
   }
 
   get player(){
-    return this.xIsNext ? 'AMINA' : 'MIRZET';
+    return this.xIsNext ? 'X' : 'O';
   }
 
   makeMove(idx: number){
--- PinkTacToe/src/app/square/square.component.ts
+++ PinkTacToe/src/app/square/square.component.ts
@@ -3,14 +3,14 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template:`
-<button [ngClass]="{'no-padding': value === 'AMINA' || value === 'MIRZET'}">
-  <img *ngIf="value === 'AMINA'" src="https://i.ibb.co/59N9d7F/Clipped-image-20231120-222209.png" alt="AMINA" style="width: 90px; height: 90px;">
-  <img *ngIf="value === 'MIRZET'" src="https://i.ibb.co/McmhDF2/Clipped-image-20231120-222226.png" alt="MIRZET" style="width: 90px; height: 90px;">
+<button [ngClass]="{'no-padding': value === 'X' || value === 'O'}">
+  <img *ngIf="value === 'X'" src="https://i.ibb.co/f4rqPgG/Picsart-23-11-21-13-29-54-346.png" alt="X" style="width: 90px; height: 90px;">
+  <img *ngIf="value === 'O'" src="https://i.ibb.co/xzR0RkK/Picsart-23-11-21-13-31-54-211.png" alt="O" style="width: 90px; height: 90px;">
 </button>
 
 `,
   styleUrl: 'square.component.css'
 })
 export class SquareComponent {
-  @Input() value: 'AMINA' | 'MIRZET';
+  @Input() value: 'X' | 'O';
 }
